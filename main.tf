aaa8327a827e1e7bdc49bfdbf473f5940db660d1
EC2 Default User <ec2-user@ip-172-31-6-120.us-east-2.compute.internal>
2025-07-31 02:49:23 +0000

    tedst

--- main.tf
+++ main.tf
@@ -5,21 +5,21 @@ provider "aws" {
 module "master" {
   source = "./modules/ec2-instance"
 
-  name           = "master"
-  instance_type  = var.instance_type
-  key_name       = var.key_name
-  vpc_id         = var.vpc_id
-  subnet_id      = var.subnet_id
-  ami_id         = var.ami_id
+  name          = "master"
+  instance_type = var.instance_type
+  key_name      = var.key_name
+  vpc_id        = var.vpc_id
+  subnet_id     = var.subnet_id
+  ami_id        = var.ami_id
 }
 
 module "worker" {
   source = "./modules/ec2-instance"
 
-  name           = "worker"
-  instance_type  = var.instance_type
-  key_name       = var.key_name
-  vpc_id         = var.vpc_id
-  subnet_id      = var.subnet_id
-  ami_id         = var.ami_id
+  name          = "worker"
+  instance_type = var.instance_type
+  key_name      = var.key_name
+  vpc_id        = var.vpc_id
+  subnet_id     = var.subnet_id
+  ami_id        = var.ami_id
 }
